ba68293d0d6cf53f5880efa781d6ef89bf367e2b
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2026-03-31 14:53:53 +0100

    add initial setup for the backend

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({ origin: process.env.CLIENT_URL }));
+
+export default app;
