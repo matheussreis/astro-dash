203aadfb701bab91d3d640fcafae17c99c7a1be3
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2026-04-01 10:28:49 +0100

    add navbar with logo and theme toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,12 @@
+import Navbar from './components/Navbar';
+
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Navbar />
+      <main className="grow min-h-[calc(100vh-4rem)] bg-primary p-6">
+        <div>Hello World!</div>
+      </main>
+    </>
+  );
 }
