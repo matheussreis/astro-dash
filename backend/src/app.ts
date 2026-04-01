b2404d778f28f5b74074c0ff04139b69143857ac
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2026-04-01 17:31:48 +0100

    add helmet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
+
+app.use(helmet());
 
 app.use(express.json());
 
