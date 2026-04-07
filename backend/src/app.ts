363b6f510285181e1a3417d232d5c89a79da617e
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2026-04-07 13:16:36 +0100

    install and set up rate limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,13 @@ import express from 'express';
 import helmet from 'helmet';
 import { FeedRouter } from './routes/index.js';
 import { clientConfig } from './config/index.js';
+import { limiter } from './utils/rate-limit.js';
 
 const app = express();
+
+app.set('trust proxy', 1);
+
+app.use(limiter);
 
 app.use(helmet());
 
---
+++ backend/src/utils/rate-limit.ts
@@ -0,0 +1,31 @@
+import logger from './logger.js';
+import rateLimit from 'express-rate-limit';
+import type { Request, Response } from 'express';
+
+const TIME_WINDOW_IN_MINUTES = 30;
+
+const TIME_WINDOW_IN_MILLISECONDS = TIME_WINDOW_IN_MINUTES * 60 * 1000;
+
+const REQUESTS_PER_TIME_WINDOW = 100;
+
+const ALLOW_X_RATE_LIMIT_HEADER = false;
+
+const STANDARD_RATE_LIMIT_HEADERS = 'draft-8';
+
+const IPV6_SUBNET_CHECK_RANGE = 56;
+
+const MESSAGE = `Too many requests, please try again in ${TIME_WINDOW_IN_MINUTES} minutes.`;
+
+const rateLimitHandler = (req: Request, res: Response) => {
+  logger.error(`Rate limit hit for IP: ${req.ip}.`);
+  res.status(429).json({ message: MESSAGE });
+};
+
+export const limiter = rateLimit({
+  windowMs: TIME_WINDOW_IN_MILLISECONDS,
+  limit: REQUESTS_PER_TIME_WINDOW,
+  standardHeaders: STANDARD_RATE_LIMIT_HEADERS,
+  legacyHeaders: ALLOW_X_RATE_LIMIT_HEADER,
+  ipv6Subnet: IPV6_SUBNET_CHECK_RANGE,
+  handler: rateLimitHandler,
+});
