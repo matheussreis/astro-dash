055c9d9373708e5a7c8589cc191a954ce6710907
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2026-04-08 17:37:32 +0100

    add button in navbar to scroll to top

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,21 @@
+import { Button } from './ui/Button';
 import { ModeToggle } from './ModeToggle';
 
 export default function Navbar() {
+  const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+
   return (
     <header className="h-15 w-full sticky top-0 bg-background select-none">
       <nav className="flex h-15 items-center justify-between px-4 md:px-6">
         <span>
           <img src="/src/assets/images/logo.png" alt="Logo" className="h-12" />
         </span>
-        <ModeToggle />
+        <span className="flex flex-row items-center justify-center gap-2">
+          <Button variant="outline" onClick={scrollToTop}>
+            Scroll to Top
+          </Button>
+          <ModeToggle />
+        </span>
       </nav>
     </header>
   );
